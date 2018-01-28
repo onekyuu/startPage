6fdd6cb18420b0810ac4401c8bd19b6af2500c52
wky0615 <wky_0615@163.com>
2018-01-28 17:02:07 +0800

    添加图标

--- main.js
+++ main.js
@@ -8,41 +8,95 @@ var hash = {
   'q': 'qq.com',
   'w': 'weibo.com',
   'e': 'ele.me',
-  'r': 'renren.com',
+  'r': 'sns.renren.com',
   't': 'tianya.com',
   'y': 'youtube.com',
-  'u': 'uc.com',
+  'u': 'uc.cn',
   'i': 'iqiyi.com',
   'o': 'opera.com',
   'p': 'pixiv.com',
   'a': 'acfun.tv',
-  's': 'sohu.com',
+  's': 'sogou.com',
+  'd': 'douban.com',
+  'f': 'ifeng.com',
+  'g': 'github.com',
+  'h': 'firefox.com',
+  'j': 'jianshu.com',
+  'k': 'kaixin001.com',
+  'l': 'linkedin.com',
   'z': 'zhihu.com',
-  'm': 'www.mcdonalds.com.cn'
+  'x': 'xiami.com',
+  'c': 'cctv.com',
+  'v': 'v2ex.com',
+  'b': 'bilibili.com',
+  'n': '',
+  'm': 'xiaomi.com'
 }
+var iconSrc = {
+  'q': 'qq',
+  'w': 'weibo',
+  'e': 'eleme',
+  'r': 'renren',
+  't': 'tianya',
+  'y': 'youtube',
+  'u': 'uc',
+  'i': 'iqiyi',
+  'o': 'opera',
+  'p': 'pixiv',
+  'a': 'acfun',
+  's': 'sogou',
+  'd': 'douban',
+  'f': 'fenghuang',
+  'g': 'github',
+  'h': 'firefox',
+  'j': 'jianshu',
+  'k': 'kaixin',
+  'l': 'linkedin',
+  'z': 'zhihu',
+  'x': 'xiami',
+  'c': 'cctv',
+  'v': 'v2ex',
+  'b': 'bilibili',
+  'n': '',
+  'm': 'xiaomi'
+}
+//发现localStorage变更，覆盖原hash
+var hashInLocalStorage = JSON.parse(localStorage.getItem('newPage') || 'null')
+if(hashInLocalStorage){
+  hash = hashInLocalStorage
+}
+//遍历keys，生成kbd标签
 for (var index = 0; index < keys.length; index++) {
-  div1 = document.createElement('div')
+  var div1 = document.createElement('div')
   kbdArea.appendChild(div1)
-  row = keys[index]
+  var row = keys[index]
   for (var index2 = 0; index2 < row.length; index2++) {
-    kbds = document.createElement('kbd')
+    var kbds = document.createElement('kbd')
     kbds.innerText = row[index2]
-    editButton = document.createElement('button')
+    var editButton = document.createElement('button')
     editButton.innerText = 'Edit'
     editButton.id = row[index2]
-    editButton.onclick = function(e){
-      console.log(e.target)
-      keyID = e.target.id
-      webLocation = prompt('请输入网址')
-      hash[keyID] = webLocation
+    //添加图标
+    var iconName = iconSrc[row[index2]]
+    var icon = document.createElement('img')
+    if(iconName){
+      icon.src = './img/'+iconName+'.png'
     }
+    //点击Edit，设置网址
+    editButton.onclick = function(e){
+      keyID = e.target.id
+      var webLocation = prompt('请输入网址')
+      hash[keyID] = webLocation
+      //localStorage变更
+      localStorage.setItem('newPage', JSON.stringify(hash))
+    }
+    kbds.appendChild(icon)
     kbds.appendChild(editButton)
     div1.appendChild(kbds)
   }
 }
 document.onkeypress = function(keyPress){
-  key = keyPress['key']
-  website = hash[key]
-  console.log(website)
-  window.open('http://' + website,'_blank')
+  var key = keyPress['key']
+  var website = hash[key]
+  window.open('http://' + website, '_blank')
 }
